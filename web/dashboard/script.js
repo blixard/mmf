54c777007782672f99e8a8ba8766a1b1eadb7d1d
Shubha <rupelpadhy@gmail.com>
2022-09-17 22:05:29 +0530

    dashboard working good

--- web/dashboard/script.js
+++ web/dashboard/script.js
@@ -38,20 +38,62 @@
     const usersRef = ref(getDatabase());
     get(child(usersRef, 'users')).then((snapshot) => {
     if (snapshot.exists()) {
-        console.log(snapshot.val()['4rupelPadhy']);
+        console.log(snapshot.val());
         for(var key in snapshot.val()){
+            var user = snapshot.val()[key]
+            console.log(user)
             const card = document.createElement("div");
             card.className = "card"
+            card.id = "key"
+            var countDownDate = new Date(user['sed']).getTime();
+            var counter = document.createElement("div")
+            var x = setInterval(function() {
+
+                // Get today's date and time
+                var now = new Date().getTime();
+              
+                // Find the distance between now and the count down date
+                var distance = countDownDate - now;
+              
+                // Time calculations for days, hours, minutes and seconds
+                var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+              
+                // Display the result in the element with id="demo"
+                
+                counter.innerHTML = days + "d " + hours + "h "
+                + minutes + "m " + seconds + "s ";
+              
+                if(days < 10 ){
+                    card.style.backgroundColor = "rgb(255, 204, 204)"
+                }
+                else{
+                    card.style.backgroundColor = "rgb(204, 255, 204)"
+                }
+                // If the count down is finished, write some text
+                if (distance < 0) {
+                  clearInterval(x);
+                  document.getElementById("demo").innerHTML = "EXPIRED";
+                }
+              }, 1000);
             card.innerHTML = `
-            <img src="./static/imgs/defaultimage.png" alt="Image" >
+            <img src="./static/imgs/defaultimage.png" alt="image" >
             <div class="container">
-              <h2>${snapshot.val()[key].firstname}</h2>
-              <p class="title">CEO &amp; Founder</p>
-              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-              <p>example@example.com</p>
-              <p><button class="button">Contact</button></p>
+              <h2>${user['firstname'] +" "+ user['lastname'] }</h2>
+              <p class="title">MMF Member</p>
+              <p class="invoiceNo">Invoice Number : ${user['invoiceno']} </p>
+              <p class="doj">Joining Date : ${user['doj']} </p>
+              <p class="sed">End date : ${user['sed']} </p>
+              <p class="piad">Amount Paid: ${user['paid']}</p>
+              <p class="due">Amount Due: ${3000-user['paid']}</p>
+              <p>Address: ${user['address']} </p>
+              <p>Email: ${user['email']}</p>
+              <a href="tel:+${user['phonenumber']}">Phone Nmber: ${user['phonenumber']}</a>
             </div>
             `
+            card.appendChild(counter)
             document.getElementById("memberslist").appendChild(card)
         }
 
